f3fc9cfb6c8316c133481e9a79b140bfb6e54395
Toshiki Baba <yz.t0.r33@gmail.com>
2019-03-13 22:45:48 +0900

    tyoeormの設定

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Connection } from 'typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+
+export class AppModule {
+  constructor(private readonly connection: Connection) {}
+}
