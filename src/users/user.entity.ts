0b15d6f535cd3274dcb4ff2fa67e2f49e72fb8ea
Toshiki Baba <yz.t0.r33@gmail.com>
2019-03-13 23:00:00 +0900

    user.entity.ts 作成

---
+++ src/users/user.entity.ts
@@ -0,0 +1,16 @@
+import { Entity, Column, PrimaryGeneratedColumn, BaseEntity } from 'typeorm';
+
+@Entity()
+export class User extends BaseEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  password: string;
+
+  @Column()
+  email: string;
+}
