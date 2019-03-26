41656adc6cc6e7d1839d8eb897d0eba71f98928e
Toshiki Baba <yz.t0.r33@gmail.com>
2019-03-27 04:53:31 +0900

    labelのスキーマ更新

--- src/labels/label.entity.ts
+++ src/labels/label.entity.ts
@@ -5,6 +5,8 @@ import {
   BaseEntity,
   ManyToOne,
   JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn
 } from 'typeorm'
 import { User } from '../users/user.entity'
 
@@ -16,7 +18,13 @@ export class Label extends BaseEntity {
   @Column({ name: 'name' })
   name: string
 
-  @ManyToOne(type => User)
+  @ManyToOne(type => User, user => user.id)
   @JoinColumn({ name: 'user_id' })
   user: User
+
+  @CreateDateColumn({ name: 'created_at' })
+  createdAt: Date
+
+  @UpdateDateColumn({ name: 'updated_at' })
+  updatedAt: Date
 }
