b2ae5e1ba3b0d5722569e2e2753336307578c7a4
Toshiki Baba <yz.t0.r33@gmail.com>
2019-04-14 18:27:23 +0900

    ActiveRecordっぽくかいたよ

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,6 +1,4 @@
 import { Injectable } from '@nestjs/common'
-import { InjectRepository } from '@nestjs/typeorm'
-import { Repository } from 'typeorm'
 import { RecordInvalidException } from '../exception'
 import { User } from './user.entity'
 import { CreateUserDto, UpdateUserDto, GoogleUserDto } from './dto/index.dto'
@@ -8,33 +6,33 @@ import { Label } from '../labels/label.entity'
 
 @Injectable()
 export class UsersService {
-  constructor(
-    @InjectRepository(User)
-    private readonly userRepository: Repository<User>,
-  ) {}
+  constructor() {}
 
   async findAll(): Promise<User[]> {
-    return await this.userRepository.find()
+    return await User.find()
   }
 
   async findById(id: number): Promise<User | undefined> {
-    return await this.userRepository.findOne(id)
+    return await User.findOne(id)
   }
 
   async findByEmail(email: string): Promise<User | undefined> {
-    return await this.userRepository.findOne({ email: email })
+    return await User.findOne({ email: email })
   }
 
   async create(dto: CreateUserDto): Promise<User> {
-    await this.userRepository.insert(dto).catch(e => {
+    const user = new User()
+    user.email = dto.email
+    user.password = dto.password
+    user.name = dto.name
+    return user.save().catch(e => {
       console.log(e)
       throw new RecordInvalidException(e.detail)
     })
-    return dto as User
   }
 
   async createWithGoogle(dto: GoogleUserDto): Promise<User> {
-    await this.userRepository.insert(dto).catch(e => {
+    await User.insert(dto).catch(e => {
       console.log(e)
       throw new RecordInvalidException(e.detail)
     })
@@ -42,7 +40,7 @@ export class UsersService {
   }
 
   async update(id: number, dto: UpdateUserDto): Promise<User> {
-    await this.userRepository.update(id, dto).catch(e => {
+    await User.update(id, dto).catch(e => {
       console.log(e)
       throw new RecordInvalidException(e.detail)
     })
@@ -50,14 +48,14 @@ export class UsersService {
   }
 
   delete(id: number): void {
-    this.userRepository.delete(id).catch(e => {
+    User.delete(id).catch(e => {
       console.log(e)
       throw new RecordInvalidException(e.detail)
     })
   }
 
   async findLabels(id: number): Promise<Label[]> {
-    const user = await this.userRepository.findOne(id, { relations: ['labels'] })
+    const user = await User.findOne(id, { relations: ['labels'] })
     return user.labels
   }
 }
