745e44badbbd21eef71e80608662ebcfb1057a67
Toshiki Baba <yz.t0.r33@gmail.com>
2019-04-15 09:36:59 +0900

    改行

--- src/issue-boxes/issue-boxes.service.ts
+++ src/issue-boxes/issue-boxes.service.ts
@@ -15,7 +15,10 @@ export class IssueBoxesService {
   ]
 
   async findByUser(user: User): Promise<IssueBox[]> {
-    return await IssueBox.find({ where: [{ user: user }], relations: ['issues', 'issues.labels'] })
+    return await IssueBox.find({
+      where: [{ user: user }],
+      relations: ['issues', 'issues.labels']
+    })
   }
 
   async create(issueBox: IssueBox): Promise<IssueBox> {
