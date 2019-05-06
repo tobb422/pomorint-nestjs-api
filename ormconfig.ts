ad0bb2f4899aebde93479917e48f21102ff97570
Toshiki Baba <yz.t0.r33@gmail.com>
2019-05-07 08:24:52 +0900

    pg-connection-string導入

--- ormconfig.ts
+++ ormconfig.ts
@@ -1,10 +1,15 @@
+import * as PostgressConnectionStringParser from "pg-connection-string"
+
+const databaseUrl: string = process.env.DATABASE_URL
+const connectionOptions = PostgressConnectionStringParser.parse(databaseUrl)
+
 export default {
   "type": "postgres",
-  "host": process.env.DB_HOST,
+  "host": connectionOptions.host,
   "port": 5432,
-  "username": process.env.DB_USERNAME,
-  "password": process.env.DB_PASSWORD,
-  "database": process.env.DB_NAME,
+  "username": connectionOptions.user,
+  "password": connectionOptions.password,
+  "database": process.env.DATABASE_NAME,
   "entities": ["src/**/**.entity{.ts,.js}"],
   "migrations": ["src/db/migrations/**/**.ts"],
   "cli": {
